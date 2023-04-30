cadf4dac143f63577b3a3a6b505c6cd97e59df33
el gato <afgutierrez4@gmail.com>
2023-04-30 12:26:04 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+### This is a set of script for prompt engineering
+
+This set of scripts are part of a short course on prompt engineering offered by Open Ai and deeplearning.ai
+
+#### answer.py
+This script answer questions and is a practice of one of the principles of prompt engineering; "Write clear and especific instructions". Espesifically, the tactic number one; "Use delimiters".
---
+++ answer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import sys
+import openai
+
+#lines below are to add api key as an env variable
+#import os
+#from dotenv import load_dotenv, find_dotenv_ = load_dotenv(find_dotenv())
+#openai.api_key  = os.getenv('OPENAI_API_KEY')
+
+openai.api_key = "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"
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+def default():
+	print("Empty!")
+
+def execute():
+	entra = sys.argv[1]
+	text = f"""{entra}"""
+	prompt = f"""Answer the question delimited by the backticks. ```{text}```"""
+	response = get_completion(prompt)
+	print(response) #FOR CONTROL
+
+def main():
+	if len(sys.argv) < 2:	
+		default()
+	else:
+		execute()
+
+if __name__ == "__main__":
+	main()
