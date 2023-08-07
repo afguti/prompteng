b00d5412055c1f041ebe69c9f9b8a7192829d3da
guti10501 <afgutierrez4@gmail.com>
2023-08-07 17:42:39 +0900

    removing comments

--- chatbot.py
+++ chatbot.py
@@ -38,12 +38,7 @@ def listen_for_input():
 def execute():
     with open("./systems/system", "r") as file:
         text = file.read()
-    text = re.sub(r'\n\s*\n', '\n', text)
-    lines = text.split("\n")
-    for i in range(len(lines)):
-        if len(lines[i]) > 0 and not re.match(r'\W', lines[i][-1]):
-            lines[i] += "."
-    text = " ".join(lines)
+    
     context = [{'role':'system', 'content':f"""{text}"""}]
     while True:
         thread = threading.Thread(target=listen_for_input)
@@ -53,11 +48,12 @@ def execute():
         context.append({'role':'user', 'content':f"{user_input}"})
         response = get_completion_from_message(context, temperature=0.3)
         #tts goes here
+
+        context.append({'role':'assistant', 'content':f"{response}"})
+        print("Chatbot: ", response)
         answ = response.replace('"','\\"')
         command = f'mimic3 --voice en_US/vctk_low \"{answ}\"'
         subprocess.run(command, shell=True)
-        context.append({'role':'assistant', 'content':f"{response}"})
-        print("Chatbot: ", response)
 
 def main():
     execute()
